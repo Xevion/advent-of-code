cd84a0ac04c72c160998729890168388dca377f6
Xevion <xevioni@yandex.com>
2019-11-23 02:31:49 -0600

    properly finish 2015 day 4 part 1 & part 2

--- 2015/day-4/python/main.py
+++ 2015/day-4/python/main.py
@@ -4,13 +4,20 @@ import sys
 import hashlib
 
 PATH = os.path.join(sys.path[0], '..', 'input')
-SALT = 'bgvyzdsv'
-md5 = hashlib.md5()
+SALT = open(PATH).read()
+md5_original = hashlib.md5(SALT.encode())
 
-i = 0
-while True:
-    md5.update((SALT + str(i)).encode())
-    if md5.hexdigest().startswith('00000'):
-        print(i)
-        break
-    i += 1
+
+def find_key(START_WITH):
+    i = 0
+    while True:
+        md5 = md5_original.copy()
+        md5.update(str(i).encode())
+        md5hash = md5.hexdigest()
+        if md5hash.startswith(START_WITH):
+            print(i)
+            break
+        i += 1
+
+find_key('00000')
+find_key('000000')
